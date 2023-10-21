23b2ddb6689ea62ad0ff13e90a5babef31ac3852
Cynthia Shen <shencynthia04@gmail.com>
2023-10-21 19:19:32 -0400

    set up tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-pink min-h-screen flex items-center justify-center">
+      <div className="p-100 bg-blue-100 shadow-lg rounded-lg">hello</div>
     </div>
   );
 }
