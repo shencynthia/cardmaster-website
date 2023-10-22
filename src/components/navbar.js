30b8a95fa3b8f0ca9f64436eb536c6a8c603745e
Inesh Tickoo <inesh.tickoo@gmail.com>
2023-10-22 00:35:46 -0400

    landing, credit cards

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,15 @@
-import logo from "../logo.png";
+import logo from "../logo.svg";
 
 function Nav() {
   return (
-    <div className="container mx-auto flex justify-between items-center">
-        <img src={logo} alt = "logo" className=""/> 
+    <div className="container mx-auto flex justify-between items-center w-[90%] h-[10%] p-[1rem]">
+        <img src={logo} alt = "logo" className="h-[50%]"/> 
         <ul class="flex space-x-4">
-        <li><a href="#" class="text-black">Home</a></li>
-        <li><a href="#" class="text-black">About</a></li>
-        <li><a href="#" class="text-black">Services</a></li>
-        <li><a href="#" class="text-black   ">Contact</a></li>
-    </ul>
+            <li><a href="#" className="text-black">Home</a></li>
+            <li><a href="#" className="text-black">About</a></li>
+            <li><a href="#" className="text-black">Services</a></li>
+            <li><a href="#" className="text-black">Contact</a></li>
+        </ul>
     </div>
   );
 }
